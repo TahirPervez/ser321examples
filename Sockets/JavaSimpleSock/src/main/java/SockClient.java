c4a09eb59cdb50c42dda3b11c3509b293f1c5d3d
TahirPervez <tpervez@asu.edu>
2021-02-17 16:23:39 -0700

    in class

--- Sockets/JavaSimpleSock/src/main/java/SockClient.java
+++ Sockets/JavaSimpleSock/src/main/java/SockClient.java
@@ -1,36 +1,27 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+import org.json.*;
 
 /**
  * A class to demonstrate a simple client-server connection using sockets.
  * Ser321 Foundations of Distributed Software Systems
  * see http://pooh.poly.asu.edu/Ser321
+ *
  * @author Tim Lindquist Tim.Lindquist@asu.edu
- *         Software Engineering, CIDSE, IAFSE, ASU Poly
+ * Software Engineering, CIDSE, IAFSE, ASU Poly
  * @version April 2020
- * 
  * @modified-by David Clements <dacleme1@asu.edu> September 2020
+ * @modified-by Kevin Moore <klmoor21@asu.edu> February 2021
  */
 class SockClient {
-  public static void main (String args[]) {
+  public static void main(String args[]) {
     Socket sock = null;
     String host = "localhost";
-    String message = "HI";
-    Integer number = 100;
+    String message = "";
+    Integer number = 0;
+    Scanner scanner = new Scanner(System.in);
 
-    // works with no inputs or 1, 2 or 3
-    // no error handling for wrong arguments
-    if (args.length >= 1){ // host, if provided
-      host=args[0];
-    }
-    if (args.length >= 2){
-      message = args[1];
-    }
-    // user provided message and number, ignore 2 arguments 
-    if (args.length >= 3){ 
-      number = Integer.valueOf(args[2]);
-    }
-    
     try {
       // open the connection
       sock = new Socket(host, 8888); // connect to host and socket on port 8888
@@ -38,16 +29,65 @@ class SockClient {
       OutputStream out = sock.getOutputStream();
       // create an object output writer (Java only)
       ObjectOutputStream os = new ObjectOutputStream(out);
-      // write the whole message
-      os.writeObject( message);
-      os.writeObject( number);
-      // make sure it wrote and doesn't get cached in a buffer
-      os.flush();
-
       ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
-      String i = (String) in.readObject();
-      System.out.println(i);
-      sock.close(); // close socked after sending
-    } catch (Exception e) {e.printStackTrace();}
+
+      serverConnection : while (true) {
+		  System.out.println("Enter 1 to enter a String, 2 to enter a Number, or 0 to Exit.");
+		  int choice = scanner.nextInt();
+		  scanner.nextLine();
+		  switch(choice) {
+			  case 1:
+				System.out.print("Please enter a String to send to the Server (enter \"exit\" to quit\"): ");
+				message = scanner.nextLine();
+				os.writeObject("{'type':'message', 'value':" + message + "'}'");
+				break;
+			  case 2:
+			    System.out.print("Please enter a Number to send to the Server (enter 0 to quit\"): ");
+				number = scanner.nextInt();
+				scanner.nextLine();
+				os.writeObject("{'type':'message', 'value':" + number + "'}'");
+				break;
+			  case 0:
+				System.out.println("EXIT");
+				os.writeObject("{'type':'exit'}");
+				break serverConnection;
+		  }
+		  
+        String response = (String) in.readObject();
+		JSONObject json = new JSONObject(response);
+		if(json.getBoolean("ok")) {
+			if(json.getString("type").equals("message")) {
+				System.out.println("Here is your return : " + json.getString("value"));
+			} else if(json.getString("type").eqsuals("message")) {
+				System.out.println("Here is your return : " + json.getInt("value"));
+			} else if (json.getString("type").equals("exit")) {
+				System.out.println("BYE");
+			}
+		} else {
+			System.out.println("WRONG");
+		}
+	  }
+
+
+/* 
+        //if sever is exiting, exit as well
+        if (i.equalsIgnoreCase("exiting")) {
+          System.out.println(i);
+          break;
+        }
+
+        System.out.println(i);
+      } */
+
+      //close resources
+      scanner.close();
+      os.close();
+      in.close();
+      sock.close();
+    } catch(ConnectException e){
+      System.out.println("Connection Error");
+    }catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
--- Sockets/JavaSimpleSock/src/main/java/SockServer.java
+++ Sockets/JavaSimpleSock/src/main/java/SockServer.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import org.json.*;
 
 /**
  * A class to demonstrate a simple client-server connection using sockets.
@@ -7,43 +8,103 @@ import java.io.*;
  * see http://pooh.poly.asu.edu/Ser321
  * @author Tim Lindquist Tim.Lindquist@asu.edu
  *         Software Engineering, CIDSE, IAFSE, ASU Poly
- * @version August 2020
+ * @version February 2021
  * 
  * @modified-by David Clements <dacleme1@asu.edu> September 2020
+ * @modified-by Kevin Moore <klmoor21@asu.edu> February 2021
  */
 public class SockServer {
   public static void main (String args[]) {
-    Socket sock;
+    Socket sock = null;
+    Boolean connected = false;
+    ObjectInputStream in = null;
+    OutputStream out = null;
+    ObjectOutputStream os = null;
+    String receviedString = "";
+    Integer receivedInt = 0;
     try {
-      //open socket
-      ServerSocket serv = new ServerSocket(8888); // create server socket on port 8888
-      System.out.println("Server ready for 3 connections");
-      // only does three connections then closes
-      // NOTE: SINGLE-THREADED, only one connection at a time
-      for (int rep = 0; rep < 3; rep++){
-        System.out.println("Server waiting for a connection");
-        sock = serv.accept(); // blocking wait
-        // setup the object reading channel
-        ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
+        //open socket
+        ServerSocket serv = new ServerSocket(8888); // create server socket on port 8888
+        System.out.println("Server ready for a connection");
         
-        // read in one object, the message. we know a string was written only by knowing what the client sent. 
-        // must cast the object from Object to desired type to be useful
-        String s = (String) in.readObject();
-        System.out.println("Received the String "+s);
-        // read in the number, we know it's an integer because that's the second thing sent by the client.
-        Integer i = (Integer) in.readObject();
-        System.out.println("Received the Integer "+ i);
+        //loop infinitely to get a connection and exchange messages
+        while(true){
+          
+            //if not connected try to connect
+            if(!connected){
+                System.out.println("Server waiting for a connection");
+                sock = serv.accept(); // blocking wait
+                // setup the object reading and writing channels
+                in = new ObjectInputStream(sock.getInputStream());
+                out = sock.getOutputStream();
+                os = new ObjectOutputStream(out);
+                connected = true;
+            }
 
-        // generate an output
-        // get output channel
-        OutputStream out = sock.getOutputStream();
-        // create an object output writer (Java only)
-        ObjectOutputStream os = new ObjectOutputStream(out);
-        // write the whole message
-        os.writeObject("Got it!");
-        // make sure it wrote and doesn't get cached in a buffer
-        os.flush();
+            //if input stream is initialized, read in object
+            if (in != null){
+                String jsonRequest = (String) in.readObject();
+				System.out.println(jsonRequest);
+				JSONObject json = new JSONObject(jsonRequest);
+				System.out.println("Server got a type: " + json.getString("type"));
+				if(json.getString("type").equals("message")) {
+					String message = json.getString("value");
+					System.out.println("Message was: " + message);
+					JSONObject resp = new JSONObject();
+					resp.put("ok",true);
+					resp.put("type", "message");
+					resp.put("value",message);
+					os.writeObject(resp.toString());
+				} else if (json.getString("type").equals("number")) {
+					int number = json.getInt("value");
+					System.out.println("Number was: " + number);
+					JSONObject resp = new JSONObject();
+					resp.put("ok",true);
+					resp.put("type", "number");
+					resp.put("value",number * 10);
+					os.writeObject(resp.toString());
+				} else if (json.getString("type").equals("exit")) {
+					JSONObject resp = new JSONObject();
+					resp.put("ok", true);
+					resp.put("type", "exit");
+					os.close();
+					in.close();
+					sock.close();
+					connected = false;
+				}
+
+            }
+
+
+
+
+
+
+
+
+
+/* 
+            //if ouput stream is initialized write output
+            if (out != null && os != null){
+
+                //if the string received is exit or integer received is 0 exit
+                if(receviedString.equalsIgnoreCase("exit") || receivedInt == 0){
+                
+                  os.writeObject("exiting");
+                  //close the streams and socket server
+                os.close();
+                in.close();
+                sock.close();
+                connected = false;
+                
+                }else{
+                  os.writeObject(receivedInt + " and " + receviedString + " ... Got it!");
+                }
+            } */
+        }  
+      } catch(Exception e) {
+        e.printStackTrace();
+        
       }
-    } catch(Exception e) {e.printStackTrace();}
   }
 }
